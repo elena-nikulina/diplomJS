98b687f194c7ad0adf8a4b4236176e297e8ef4de
elena-nikulina <info@webstar-support.com>
2022-05-13 00:09:45 +0700

    Сделала закрытие модалки

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,22 +2,44 @@ const modal = () => {
     const modal1 = document.querySelector('.modal-callback');
     const overlay = document.querySelector('.modal-overlay');
     const callbackBtn = document.querySelectorAll('.callback-btn');
+    const modalClose = modal1.querySelector('.modal-close');
 
-    console.log(modal1);
-    console.log(overlay);
-    console.log(callbackBtn);
+    //let isOpen = false;
+
+    //console.log(modal1);
+    //console.log(overlay);
+    //console.log(callbackBtn);
+    console.log(modalClose);
 
     for(let btn of callbackBtn) {
         if (btn.classList.contains('fancyboxModal')) {
             btn.classList.remove('fancyboxModal');
         }
         btn.addEventListener('click', () => {
-            
+            //isOpen = true;
             console.log('click');
             modal1.style.display = 'block';
             overlay.style.display = 'block';
         });
+
+       
     }
+
+    
+        modalClose.addEventListener('click', () => {
+            console.log('none1');
+            modal1.style.display = 'none';
+            overlay.style.display = 'none';
+        });
+    
+        overlay.addEventListener('click', () => {
+            modal1.style.display = 'none';
+            overlay.style.display = 'none';
+        });
+    
+    
+
+    
 }
 
 //module.exports = twoModule;
